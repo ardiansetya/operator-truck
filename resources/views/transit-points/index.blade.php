f44472b67f4075238b7b7e75490b962eb28f0ef8
ardiansetya <ardiansetya129@gmail.com>
2025-08-13 14:59:22 +0700

    change hapus to nonaktifkan in transit point

--- resources/views/transit-points/index.blade.php
+++ resources/views/transit-points/index.blade.php
@@ -179,7 +179,8 @@
                                 </svg>
                                 <span>Edit</span>
                             </a>
-                            <form action="{{ route('transit-points.destroy', $transitPoint['id']) }}" method="POST"
+                          @if ($transitPoint['is_active'])
+                                <form action="{{ route('transit-points.destroy', $transitPoint['id']) }}" method="POST"
                                 class="flex-1"
                                 onsubmit="return confirm('Apakah Anda yakin ingin menghapus transit point ini?')">
                                 @csrf
@@ -191,9 +192,10 @@
                                             d="M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3M4 7h16">
                                         </path>
                                     </svg>
-                                    <span>Hapus</span>
+                                    <span>Nonaktifkan</span>
                                 </button>
                             </form>
+                          @endif
                         </div>
                     </div>
                 </div>
